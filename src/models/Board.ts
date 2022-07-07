fdaf6a63404405af4aadc0f05a9276c0fb4fc641
SoinRoma <soinroma26@gmail.com>
2022-07-07 13:37:53 +0500

    - add method addPawns

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -1,6 +1,6 @@
 import {Cell} from "./Cell";
 import {Colors} from "./Colors";
-import {Queen} from "./figures/Queen";
+import {Pawn} from "./figures/Pawn";
 
 export class Board {
     cells: Cell[][] = [];
@@ -11,7 +11,7 @@ export class Board {
         for (let i = 0; i < 8; i++) {
             const row: Cell[] = [];
             for (let j = 0; j < 8; j++) {
-                if((i+j)%2 !==0){
+                if ((i + j) % 2 !== 0) {
                     row.push(new Cell(this, j, i, Colors.BLACK, null)); // Черные ячейки
                 } else {
                     row.push(new Cell(this, j, i, Colors.WHITE, null)); // Белые ячейки
@@ -21,11 +21,52 @@ export class Board {
         }
     }
 
-    public getCell(x:number, y:number){
+    // Получение координат
+    public getCell(x: number, y: number) {
         return this.cells[y][x]
     }
 
-    public addFigures(){
-        new Queen(Colors.WHITE, this.getCell(3, 6))
+    // Добавление пешок
+    private addPawns() {
+        for (let i = 0; i < 8; i++) {
+            new Pawn(Colors.BLACK, this.getCell(i, 1));
+            new Pawn(Colors.WHITE, this.getCell(i, 6));
+        }
+    }
+
+    // Добавление коней
+    private addKnights() {
+
+    }
+
+    // Добавление кололей
+    private addKings() {
+
+    }
+
+    // Добавление ферзей
+    private addQueens() {
+
+    }
+
+    // Добавление ладьей
+    private addRocks() {
+
+    }
+
+    // Добавление слонов
+    private addBishops() {
+
+    }
+
+
+    // Добавление фигур
+    public addFigures() {
+        this.addPawns();
+        this.addBishops();
+        this.addKings();
+        this.addKnights();
+        this.addQueens();
+        this.addRocks();
     }
 }
