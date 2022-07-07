72ce202905641d4bd18b58ee225b7dcfa126b4a1
SoinRoma <soinroma26@gmail.com>
2022-07-07 12:05:01 +0500

    - add model for board and cell

---
+++ src/models/Board.ts
@@ -0,0 +1,22 @@
+import {Cell} from "./Cell";
+import {Colors} from "./Colors";
+
+export class Board {
+    cells: Cell[][] = [];
+
+    // Инициализация ячеек в доске
+    public initCells() {
+        // Проходимя по двум циклам по строке и по столбцу
+        for (let i = 0; i < 8; i++) {
+            const row: Cell[] = [];
+            for (let j = 0; j < 8; j++) {
+                if((i+j)%2 !==0){
+                    row.push(new Cell(this, j, i, Colors.BLACK, null)); // Черные ячейки
+                } else {
+                    row.push(new Cell(this, j, i, Colors.WHITE, null)); // Белые ячейки
+                }
+            }
+            this.cells.push(row);
+        }
+    }
+}
---
+++ src/models/Cell.ts
@@ -0,0 +1,28 @@
+import {Colors} from "./Colors";
+import {Figure} from "./figures/Figure";
+import {Board} from "./Board";
+
+export class Cell {
+    // Координаты ячейки и цвет (не меняются)
+    readonly x: number;
+    readonly y: number;
+    readonly color: Colors;
+    // Фигура на ячейки (любая или ничего)
+    figure: Figure | null;
+    board: Board;
+    // Может ли фигура переместиться
+    available: boolean;
+    // Для реакт ключей
+    id: number;
+
+    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure | null) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.figure = figure;
+        this.board = board;
+        this.available = false;
+        this.id = Math.random();
+    }
+
+}
