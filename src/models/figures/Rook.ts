4348cdf0be36458dd7b8bcccfc482e5a0cd57ef8
SoinRoma <soinroma26@gmail.com>
2022-07-07 13:58:42 +0500

    - add methods for add others figures

--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -4,7 +4,7 @@ import {Cell} from "../Cell";
 import blackLogo from '../../images/black-rook.png';
 import whiteLogo from '../../images/white-rook.png';
 
-export class Rock extends Figure {
+export class Rook extends Figure {
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
