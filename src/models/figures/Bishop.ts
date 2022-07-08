89dd893f99fb438da0088d57f34661b76e62695a
SoinRoma <soinroma26@gmail.com>
2022-07-08 12:06:15 +0500

    - fix function for rook and bishop

--- src/models/figures/Bishop.ts
+++ src/models/figures/Bishop.ts
@@ -16,9 +16,13 @@ export class Bishop extends Figure {
         if (!super.canMove(target)){
             return false;
         }
-        if (this.cell.isEmptyDiagonal(target)){
+        if (this.cell.isEmptyVertical(target)){
             return true;
         }
+        if (this.cell.isEmptyHorizontal(target)){
+            return true;
+        }
+
         return false;
     }
 }
--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -15,10 +15,7 @@ export class Rook extends Figure {
         if (!super.canMove(target)){
             return false;
         }
-        if (this.cell.isEmptyVertical(target)){
-            return true;
-        }
-        if (this.cell.isEmptyHorizontal(target)){
+        if (this.cell.isEmptyDiagonal(target)){
             return true;
         }
         return false;
