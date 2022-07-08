aa74c942dd73d9e068babef2863ee8af60412f2a
SoinRoma <soinroma26@gmail.com>
2022-07-08 10:18:36 +0500

    - add function for check horizontal - add function for check vertical - add function for check diagonal - and fix function for move figure

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -25,11 +25,75 @@ export class Cell {
         this.id = Math.random();
     }
 
+    // Проверка на пустую клетку
+    isEmpty() {
+        return this.figure === null;
+    }
+
+    // Проверка вертикали на наличие вражеских фигур
+    isEmptyVertical(target: Cell): boolean {
+        // Проверяем чтобы столбы совпадали(у выделенной фигуры и других фигур)
+        if (this.x !== target.x) {
+            return false;
+        }
+
+        const min = Math.min(this.y, target.y);
+        const max = Math.max(this.y, target.y);
+        for (let y = min + 1; y < max; y++) {
+            if (!this.board.getCell(this.x, y).isEmpty()) {
+                return false
+            }
+        }
+        return true;
+    }
+
+    // Проверка горизонтали на наличие вражеских фигур
+    isEmptyHorizontal(target: Cell): boolean {
+        // Проверяем чтобы строки совпадали(у выделенной фигуры и других фигур)
+        if (this.y !== target.y) {
+            return false;
+        }
+
+        const min = Math.min(this.x, target.x);
+        const max = Math.max(this.x, target.x);
+        for (let x = min + 1; x < max; x++) {
+            if (!this.board.getCell(x, this.y).isEmpty()) {
+                return false
+            }
+        }
+        return true;
+    }
+
+    // Проверка диагонали на наличие вражеских фигур
+    isEmptyDiagonal(target: Cell): boolean {
+        const absX = Math.abs(target.x - this.x);
+        const absY = Math.abs(target.y - this.y);
+        if (absY !== absX) {
+            return false;
+        }
+
+        const dy = this.y < target.y ? 1 : -1;
+        const dx = this.x < target.x ? 1 : -1;
+
+        for (let i = 1; i < absY; i++) {
+            if (!this.board.getCell(this.x + dx * i, this.y + dy * i).isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Обновление области видимости фигуры после смены хода
+    setFigure(figure: Figure) {
+        this.figure = figure;
+        this.figure.cell = this;
+    }
+
     moveFigure(target: Cell) {
-        if(this.figure && this.figure?.canMove(target)){
+        if (this.figure && this.figure?.canMove(target)) {
             this.figure.moveFigure(target);
             // меняем фигуру на новое место, а старое место удаляем
-            target.figure = this.figure;
+            target.setFigure(this.figure);
             this.figure = null;
         }
     }
