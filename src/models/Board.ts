7c6f7c2762a52a2030def04fff60c227c2245764
SoinRoma <soinroma26@gmail.com>
2022-07-08 10:00:25 +0500

    - fix coordinate for kings and queens

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -69,13 +69,13 @@ export class Board {
     // Добавление кололей
     private addKings() {
         new King(Colors.BLACK, this.getCell(3, 0));
-        new King(Colors.WHITE, this.getCell(3, 7));
+        new King(Colors.WHITE, this.getCell(4, 7));
     }
 
     // Добавление ферзей
     private addQueens() {
         new Queen(Colors.BLACK, this.getCell(4, 0));
-        new Queen(Colors.WHITE, this.getCell(4, 7));
+        new Queen(Colors.WHITE, this.getCell(3, 7));
     }
 
     // Добавление ладьей
